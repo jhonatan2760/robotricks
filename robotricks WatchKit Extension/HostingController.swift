7a9ca864c632fe71c88469f59305833361effe82
jhonatansilvadesousa <jhonatan2760@gmail.com>
2020-05-23 14:44:33 -0300

    Initial Commit

---
+++ robotricks WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  robotricks WatchKit Extension
+//
+//  Created by Jhonatan Silva de Sousa on 23/05/20.
+//  Copyright © 2020 Jhonatan Silva de Sousa. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ robotricks WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  robotricks WatchKit Extension
+//
+//  Created by Jhonatan Silva de Sousa on 23/05/20.
+//  Copyright © 2020 Jhonatan Silva de Sousa. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
